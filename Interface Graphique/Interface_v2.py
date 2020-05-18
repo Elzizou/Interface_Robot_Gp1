984d557e718156adfb84bb69977f7760fceaeba7
sella <sellaziz@hotmail.fr>
2020-05-18 19:04:41 +0200

    update interface

--- Interface Graphique/Interface_v2.py
+++ Interface Graphique/Interface_v2.py
@@ -12,7 +12,6 @@ from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 import tkinter.scrolledtext as tkscrolledtext
 
-
 class Interface():
     # VARIABLES
     FIGSIZE = (5, 4)
@@ -72,37 +71,30 @@ class Interface():
 
     def set_buttons(self):
         """Mise en place des boutons sur Tkinter"""
-
+        self.query_frame = tkinter.Frame(self.root)
         # Create new window to add entries by hand
-        self.window_button = tkinter.Button(self.root, text="Command Window", command=self.create_window)
+        self.window_button = tkinter.Button(self.query_frame, text="Command Window", command=self.create_window)
         self.window_button.pack(side=tkinter.LEFT)
 
-        self.update_linmethodbutton = tkinter.Button(master=self.root, text="Line",
+        self.update_linmethodbutton = tkinter.Button(master=self.query_frame, text="Line",
                                                      command=self.to_Line, relief=tkinter.SUNKEN)
         self.update_linmethodbutton.pack(side=tkinter.LEFT)
 
-        self.update_cirmethodbutton = tkinter.Button(master=self.root, text="CIR",
+        self.update_cirmethodbutton = tkinter.Button(master=self.query_frame, text="CIR",
                                                      command=self.to_CIR)
         self.update_cirmethodbutton.pack(side=tkinter.LEFT)
 
-        self.erase_methodbutton = tkinter.Button(master=self.root, text="Erase",
+        self.erase_methodbutton = tkinter.Button(master=self.query_frame, text="Erase",
                                                  command=self.erase_plt)
         self.erase_methodbutton.pack(side=tkinter.LEFT)
 
-        self.pos_button = tkinter.Button(master=self.root, text="Pos", command=self.pos_meth)
+        self.pos_button = tkinter.Button(master=self.query_frame, text="Simulation", command=self.Simulation)
         self.pos_button.pack(side=tkinter.LEFT)
 
-        self.command_button = tkinter.Button(master=self.root, text="Command", command=self.print_command)
-        self.command_button.pack(side=tkinter.LEFT)
-
-        self.export_button = tkinter.Button(master=self.root, text="Export", command=self.command_txt)
+        self.export_button = tkinter.Button(master=self.query_frame, text="Export", command=self.command_txt)
         self.export_button.pack(side=tkinter.LEFT)
 
-        self.dir_button = tkinter.Button(master=self.root, text="dir", command=self.print_dir)
-        self.dir_button.pack(side=tkinter.LEFT)
-
-        self.dirtest_button = tkinter.Button(master=self.root, text="arrow dir", command=self.test_dir)
-        self.dirtest_button.pack(side=tkinter.LEFT)
+        self.query_frame.pack(side = tkinter.LEFT)
 
         self.quit_button = tkinter.Button(master=self.root, text="Quit", command=self._quit)
         self.quit_button.pack(side=tkinter.BOTTOM)
@@ -116,28 +108,6 @@ class Interface():
         self.directions = np.array([[0, 1]])
         self.drawing, = self.ax.plot(self.plt_draw[:, 0], self.plt_draw[:, 1])
 
-    def pos_meth(self):
-        """Met en place la méthode Position"""
-        self.DRAW_METHOD = 3
-
-    def print_dir(self):
-        """Affiche la liste des directions sur l'invité de commande"""
-        print(self.directions)
-
-    def test_dir(self):
-        """Affiche une flèche pour symboliser la direction de la voiture, ainsi que la normale"""
-        xold, yold = self.plt_draw[-1]
-        xdir, ydir = self.directions[-1]
-        self.ax.arrow(xold, yold, xdir, ydir, width=0.2)
-        self.ax.arrow(xold, yold, -ydir, xdir, width=0.2, color='g', alpha=0.1)
-        self.ax.figure.canvas.draw()
-
-    def to_CIR(self):
-        """Change la méthode de tracé au CIR"""
-        self.update_cirmethodbutton.config(relief=tkinter.SUNKEN)
-        self.update_linmethodbutton.configure(relief=tkinter.RAISED)
-        self.DRAW_METHOD = 2
-
     def erase_plt(self):
         """Réinitialise le tracé"""
         self.ax.cla()
@@ -149,15 +119,9 @@ class Interface():
         self.imgplot = self.ax.imshow(self.robot_im, origin=(0, 1), extent=([-1, 1, -1, 1]))
         self.ax.figure.canvas.draw()
 
-    def to_Line(self):
-        """Change la méthode de tracé au LIN"""
-        self.update_linmethodbutton.config(relief=tkinter.SUNKEN)
-        self.update_cirmethodbutton.configure(relief=tkinter.RAISED)
-        self.DRAW_METHOD = 1
-
     def on_key_press(self, event):
         """Gestion de l'évènement clic gauche et gestion du tracé"""
-        print("you pressed ({0},{1})".format(event.xdata, event.ydata))
+        # print("you pressed ({0},{1})".format(event.xdata, event.ydata))
         key_press_handler(event, self.canvas, self.toolbar)
         if event.inaxes != self.ax.axes: return
         x, y = event.xdata, event.ydata
@@ -166,44 +130,33 @@ class Interface():
             self.draw_lineinter(x, y)
         if self.DRAW_METHOD == 2:  # CIR
             self.draw_CIR(x, y)
-        if self.DRAW_METHOD == 3:
-            self.test_pos(x, y)
         # Update Canvas
         self.drawing.set_data(self.plt_draw[:, 0], self.plt_draw[:, 1])
         self.ax.figure.canvas.draw()
 
-    def create_window(self):
-        """Affiche l'invité de commande LIN,CIR,ROT"""
-        self.window = tkinter.Toplevel(self.root)
-        self.set_windowbutton()
+    # Changement de méthode de tracé
+    def to_CIR(self):
+        """Change la méthode de tracé au CIR"""
+        self.update_cirmethodbutton.config(relief=tkinter.SUNKEN)
+        self.update_linmethodbutton.configure(relief=tkinter.RAISED)
+        self.DRAW_METHOD = 2
 
-    def test_pos(self, x, y):
-        """Signalise sur l'invité de commande si on clique à gauche ou droite de la voiture"""
-        xdir, ydir = self.directions[-1][0], self.directions[-1][1]
-        xold, yold = self.plt_draw[-1]
-        print(np.dot([-ydir, xdir], [x - xold, y - yold]))
-        if np.dot([-ydir, xdir], [x - xold, y - yold]) > 0:
-            print("A gauche chef")
-        else:
-            print("A droite chef")
+    def to_Line(self):
+        """Change la méthode de tracé au LIN"""
+        self.update_linmethodbutton.config(relief=tkinter.SUNKEN)
+        self.update_cirmethodbutton.configure(relief=tkinter.RAISED)
+        self.DRAW_METHOD = 1
 
-    def get_ang(self, v1, v2, dir=False, direction=(0, 1)):
-        """Détermine l'angle de rotation avec considération possible de la direction précédante"""
-        vect1, vect2 = v1, v2
-        if dir:
-            vect2 = vect2 - vect1
-            vect1 = direction
-        v1_unit = vect1 / np.linalg.norm(vect1)
-        v2_unit = vect2 / np.linalg.norm(vect2)
-        dot_product = np.dot(v1_unit, v2_unit)
-        return np.arccos(dot_product)
+    def Simulation(self):
+        print("Simulation à implanter dans l'interface graphique?")
 
+    # Méthodes de tracé
     def ROT(self):
         """Méthode de Rotation de la voiture"""
         # Update direction
         angrad = float(self.thetaentry.get()) * np.pi * 2 / 360
         xdir, ydir = self.directions[-1]
-        new_dir = self.rotatenp(xdir, ydir, angrad)
+        new_dir = self.rotatenp(xdir, ydir, -angrad)
         new_dir = np.array([round(new_dir[0], 2), round(new_dir[1], 2)])
         self.add_direction(new_dir)
         # Add command
@@ -214,19 +167,12 @@ class Interface():
         self.ax.figure.canvas.draw()
         return 0
 
-    def rotatenp(self, x, y, ang):
-        """Use numpy to build a rotation matrix and take the dot product."""
-        co, si = np.cos(ang), np.sin(ang)
-        j = np.array([[co, si], [-si, co]])
-        m = np.dot(j, [x, y])
-        return float(m[0]), float(m[1])
-
     def LIN(self):
         """Méthode de ligne droite de la voiture"""
         d = float(self.dentry.get())
         xold, yold = self.plt_draw[-1]
         xdir, ydir = self.directions[-1]
-        print("(xold = {0}, yold = {1}, xdir = {2}, ydir = {3}".format(xold, yold, xdir, ydir))
+        # print("(xold = {0}, yold = {1}, xdir = {2}, ydir = {3}".format(xold, yold, xdir, ydir))
         if d > 0:
             xout = xold + d * xdir
             yout = yold + d * ydir
@@ -236,16 +182,13 @@ class Interface():
             self.add_direction(np.array([-self.directions[-1][0], -self.directions[-1][1]]))
         self.plt_draw = np.vstack((self.plt_draw, np.array([xout, yout])))
         self.drawing.set_data(self.plt_draw[:, 0], self.plt_draw[:, 1])
-        self.add_command(1, xout, yout)
+        self.add_command(1, d, 0)
         self.ax.figure.canvas.draw()
         return 0
 
     def CIR(self):
         """Méthode tracé d'arc de la voiture"""
-        print(self.plt_draw[-1][0], self.plt_draw[-1][1], self.xcirentry.get(), self.ycirentry.get())
         self.draw_CIR(float(self.xcirentry.get()), float(self.ycirentry.get()))
-        # self.command = np.vstack((self.command, np.array([2, self.xcirentry.get(), self.ycirentry.get()])))
-        self.add_command(2, self.xcirentry.get(), self.ycirentry.get())
 
     def draw_lineinter(self, x, y):
         """Trace le LIN de la voiture"""
@@ -255,56 +198,18 @@ class Interface():
         self.add_direction(dv)
         # Ajout des coordonnées du click
         self.plt_draw = np.vstack((self.plt_draw, np.array([x, y])))
-        if np.dot((self.directions[-1][0], 0),
-                  (1, 0)) > 0:  # Essai pour déterminer le signe de l'angle mais infructueux
-            angle = -angle
         # Ajout des commandes
         if np.abs(angle) > 0.001:  # Ajoute une commande de rotation s'il est non nul
             self.add_command(0, angle, 0)
-        self.add_command(1, x, y)
-
-    def add_command(self, c, i1, i2):
-        """Met à jour la liste de commandes"""
-        self.command = np.vstack((self.command, np.array([c, i1, i2])))
-        command_dict = {0: 'ROT', 1: 'LIN', 2: 'CIR'}
-        c = command_dict[int(self.command[-1][0])]
-        self.textbox.insert('1.0',
-                            "{0}({1},{2})\n".format(c, round(self.command[-1][1], 2), round(self.command[-1][2])), 2)
-
-    def add_direction(self, dv):
-        """Met à jour la liste de directions"""
-        self.directions = np.vstack((self.directions, dv / np.linalg.norm(dv)))
-
-    def init_arc(self, xout, yout):
-        """Crée un arc dans la base de la voiture"""
-        xin, yin = self.plt_draw[-1][0], self.plt_draw[-1][1]
-        ### Calcul dans le cas (x0,y0)=(0,0) et direction = (0,1)
-        dx, dy = xout - xin, yout - yin
-        xdir, ydir = self.directions[-1][0], self.directions[-1][1]
-        ang_b = self.get_ang((1, 0), (self.directions[-1][0], self.directions[-1][1]))
-        dxr, dyr = self.rot_inv(xin,yin, xout, yout, np.pi/2-ang_b)
-        theta_ini = self.get_ang((1, 0), (1, 0))
-
-        Xcv = dyr * np.sin(theta_ini) + dxr * np.cos(theta_ini)
-        Ycv = dyr * np.cos(theta_ini) - dxr * np.sin(theta_ini)
-        print("init_arc : Xcv = {0}, Ycv={1}".format(Xcv,Ycv))
-        if Ycv < 0: return None, None,None, None, None # On bannit le cas où Ycv<0
-        sgn = (Xcv / np.abs(Xcv))
-        R = sgn * (Xcv ** 2 + Ycv ** 2) / (2 * Xcv)
-        X = np.linspace(0, Xcv, 100)
-        Y = np.sqrt(R ** 2 - np.power(X - sgn * R, 2))
-        return Xcv, Ycv, sgn, X, Y
+        self.add_command(1, np.linalg.norm(dv), 0)
 
     def draw_CIR(self, xout, yout):
         """Trace le CIR"""
         xin, yin = self.plt_draw[-1][0], self.plt_draw[-1][1]
         Xcv, Ycv, sgn, X, Y = self.init_arc(xout, yout)
-        ang_b = self.get_ang((0, 1), self.directions[-1])
-        if np.dot((self.directions[-1][0], 0),
-                  (1, 0)) > 0:
-            ang_b = -ang_b
+        ang_b = self.get_ang((0, 1), (self.directions[-1][0], self.directions[-1][1]))
         if Xcv==None:
-            print("marche pas")
+            print("Invalid CIR Command")
             return
         ### Changement de base du cas précédent pour le mettre en modèle global
         dir_2 = self.directions[-1]
@@ -327,22 +232,33 @@ class Interface():
         self.ax.figure.canvas.draw()
         self.add_command(2, xout, yout)
 
+    def init_arc(self, xout, yout):
+        """Crée un arc dans la base de la voiture"""
+        xin, yin = self.plt_draw[-1][0], self.plt_draw[-1][1]
+        ### Calcul dans le cas (x0,y0)=(0,0) et direction = (0,1)
+        dx, dy = xout - xin, yout - yin
+        xdir, ydir = self.directions[-1][0], self.directions[-1][1]
+        ang_b = self.get_ang((1, 0), (self.directions[-1][0], self.directions[-1][1]))
+        dxr, dyr = self.rot_inv(xin,yin, xout, yout, np.pi/2-ang_b)
+        theta_ini = 0
+
+        Xcv = dyr * np.sin(theta_ini) + dxr * np.cos(theta_ini)
+        Ycv = dyr * np.cos(theta_ini) - dxr * np.sin(theta_ini)
+        # print("init_arc : Xcv = {0}, Ycv={1}".format(Xcv,Ycv))
+        if Ycv < 0: return None, None,None, None, None # On bannit le cas où Ycv<0
+        sgn = (Xcv / np.abs(Xcv))
+        R = sgn * (Xcv ** 2 + Ycv ** 2) / (2 * Xcv)
+        X = np.linspace(0, Xcv, 100)
+        Y = np.sqrt(R ** 2 - np.power(X - sgn * R, 2))
+        return Xcv, Ycv, sgn, X, Y
+
     def rot_inv(self, xin, yin, xout, yout, theta):
         dx, dy = xout - xin, yout - yin
-        print("rot_inv : dx = {0}, dy={1}".format(dx,dy))
-        print("rot_inv : theta={}".format(theta))
+        # print("rot_inv : dx = {0}, dy={1}".format(dx,dy))
+        # print("rot_inv : theta={}".format(theta))
         dxr, dyr = -np.sin(theta)*dy+np.cos(theta)*dx, np.cos(theta)*dy+np.sin(theta)*dx
-        print("rot_inv : dxr = {0}, dyr={1}".format(dxr,dyr))
+        # print("rot_inv : dxr = {0}, dyr={1}".format(dxr,dyr))
         return dxr, dyr
-
-
-    def length(self, data1, data2):
-        """calcule la longueur de la trajectoire"""
-        l = 0
-        for k in range(1, len(data2)):
-            delta = data1[k] - data1[k - 1], data2[k] - data2[k - 1]
-            l += np.linalg.norm(delta)
-        return l
 
     def changement_base(self, xrel, yrel, theta, x0, y0):
         """Change la base (rotation, translation)"""
@@ -355,19 +271,63 @@ class Interface():
         result = np.dot(chg_base, j)
         return float(result[0]), float(result[1])
 
-    def print_command(self):
-        """Imprime les commandes sur la console"""
-        print(self.command)
+    # Méthodes géométriques
+    def rotatenp(self, x, y, ang):
+        """Use numpy to build a rotation matrix and take the dot product."""
+        co, si = np.cos(ang), np.sin(ang)
+        j = np.array([[co, si], [-si, co]])
+        m = np.dot(j, [x, y])
+        return float(m[0]), float(m[1])
+
+    def get_ang(self, v1, v2, dir=False, direction=(0, 1)):
+        """Détermine l'angle de rotation avec considération possible de la direction précédante"""
+        vect1 = np.array([float(v1[0]), float(v1[1])])
+        vect2 = np.array([float(v2[0]), float(v2[1])])
+        if dir:
+            vect2 = vect2 - vect1
+            vect1 = np.array([float(direction[0]), float(direction[1])])
+        if  np.linalg.norm(vect1)* np.linalg.norm(vect2)==0:
+            print("get_ang: Invalid value")
+            return 0
+        v1_unit = vect1 / np.linalg.norm(vect1)
+        v2_unit = vect2 / np.linalg.norm(vect2)
+        dot_product = np.dot(v1_unit, v2_unit)
+        sgn = v1_unit[0]*v2_unit[1]-v1_unit[1]*v2_unit[0]
+        if sgn!=0:
+            sgn = sgn/np.abs(sgn)
+        else:
+            return 0
+        return sgn*np.arccos(dot_product)
+
+    # Méthodes de mise à jour des variables
+    def add_command(self, c, i1, i2):
+        """Met à jour la liste de commandes"""
+        self.command = np.vstack((self.command, np.array([c, i1, i2])))
+        command_dict = {0: 'ROT', 1: 'LIN', 2: 'CIR'}
+        c = command_dict[int(self.command[-1][0])]
+        self.textbox.insert('1.0',
+                            "{0}({1},{2})\n".format(c, round(self.command[-1][1], 2), round(self.command[-1][2])), 2)
+
+    def add_direction(self, dv):
+        """Met à jour la liste de directions"""
+        self.directions = np.vstack((self.directions, dv / np.linalg.norm(dv)))
 
     def command_txt(self):
         """Renvoie les commande sous forme de fichier texte, la suite du programme est géré par la partie Trajectoire"""
         path = os.getcwd()
-        text_data = open(path + "\\trajectoire.txt", "w")
+        text_data = open(path + "\\values.txt", "w")
         command_dict = {0: 'R', 1: 'L', 2: 'A'}
-        c = command_dict[int(self.command[-1][0])]
-        for instruct in self.command:
+        # print(self.command)
+        for instruct in self.command[1:-1]:
+            c = command_dict[int(instruct[0])]
             txt = "{0},{1},{2};".format(c, round(instruct[1], 3), round(instruct[2], 3))
+            # print("{0},{1},{2};".format(c, round(instruct[1], 3), round(instruct[2], 3)))
             text_data.write(txt)
+        instruct = self.command[-1]
+        c = command_dict[int(instruct[0])]
+        txt = "{0},{1},{2}".format(c, round(instruct[1], 3), round(instruct[2], 3))
+        # print("{0},{1},{2};".format(c, round(instruct[1], 3), round(instruct[2], 3)))
+        text_data.write(txt)
         text_data.close()
         return 0
 
@@ -376,6 +336,11 @@ class Interface():
         self.root.quit()  # stops mainloop
         self.root.destroy()  # this is necessary on Windows to prevent
         # Fatal Python Error: PyEval_RestoreThread: NULL tstate
+
+    def create_window(self):
+        """Affiche l'invité de commande LIN,CIR,ROT"""
+        self.window = tkinter.Toplevel(self.root)
+        self.set_windowbutton()
 
     def set_windowbutton(self):
         """Met en place la fenetre de commande"""
